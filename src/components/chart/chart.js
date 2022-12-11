ec876286eccad1a6e79e8841f64a32c647411810
Evgeniy <y.danchanka@tapston.com>
2022-12-11 15:05:28 +0300

    create chart component

---
+++ src/components/chart/chart.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Legend,
+    Tooltip
+} from "recharts";
+
+const Chart = ({data = [], width = 600, height = 400, margin = {}}) => {
+    return (
+        <>
+            <BarChart
+                width={width}
+                height={height}
+                data={data}
+                margin={margin}
+            >
+                <CartesianGrid strokeDasharray={'3 3'}/>
+                <XAxis dataKey={"name"}/>
+                <YAxis/>
+                <Tooltip/>
+                <Legend/>
+                <Bar dataKey={'курс'} fill={'#B0C4DE'}/>
+            </BarChart>
+        </>
+    );
+};
+
+export default Chart;
