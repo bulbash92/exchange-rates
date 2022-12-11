36167113fb20964444adec78b38d67465572730e
Evgeniy <y.danchanka@tapston.com>
2022-12-11 15:06:25 +0300

    create api

---
+++ src/api/api.js
@@ -0,0 +1,24 @@
+const api = `https://www.nbrb.by/api/exrates`
+
+export const CurrencyApi = {
+    getDynamicsExchange: async (id, startDate, endDate) => {
+        try {
+            const response = await fetch(`${api}/rates/dynamics/${id}?startDate=${startDate}&endDate=${endDate}`)
+            const data = await response.json()
+            return data
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
+    },
+    getCurrencyListHandler: async (checkDate) => {
+        try {
+            const response = await fetch(`${api}/rates?ondate=${checkDate}&periodicity=0`)
+            const data = await response.json()
+            return data
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
+    }
+}
