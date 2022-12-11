5234b29fb782970cb789e0341d0e5f6681395b39
Evgeniy <y.danchanka@tapston.com>
2022-12-11 15:07:32 +0300

    create layout navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './navbar.module.css'
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    const linkClassName = ({ isActive }) =>
+        isActive ? styles.active : styles.item;
+    return (
+        <nav className={styles.container}>
+            <div className={styles.item}><NavLink to={'/'} className={linkClassName}>Курсы </NavLink></div>
+            <div className={styles.item}><NavLink to={'/dynamic'} className={linkClassName}>Динамика</NavLink></div>
+            <div className={styles.item}><NavLink to={'/converter'} className={linkClassName}>Конвертер</NavLink></div>
+        </nav>
+    );
+};
+
+export default Navbar;
