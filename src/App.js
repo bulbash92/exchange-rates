88223b420c6b9186b66776c70326032d8c62c330
Evgeniy <y.danchanka@tapston.com>
2022-12-11 20:33:20 +0300

    refactor header

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import {Route, Routes} from 'react-router-dom';
 
 import ExchangeRates from "./Pages/Exchange-Rates/exchangeRates";
 import ExchangeDynamics from "./Pages/exchangeDynamics/exchangeDynamics";
-import Navbar from "./components/navbar/navbar";
-import logo
-    from '../src/assets/png-transparent-drawing-cartoon-potato-face-food-photography.png'
 import Converter from "./Pages/converter/converter";
+import Header from "./components/header/header";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <div className={'logo'}><img alt={'logo'} src={logo}/></div>
-                <h1>Бульба Курс</h1>
-                <Navbar/>
-            </header>
+            <Header/>
             <Routes>
                 <Route path='/' element={<ExchangeRates/>}/>
                 <Route path='/dynamic' element={<ExchangeDynamics/>}/>
