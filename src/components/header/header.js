a8779f549694160ea6b842073ddf906621e7eb29
Evgeniy <y.danchanka@tapston.com>
2022-12-12 11:54:26 +0300

    add info about client browser

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { browserName } from 'react-device-detect';
 import styles from './header.module.css'
 import logo
   from "../../assets/png-transparent-drawing-cartoon-potato-face-food-photography.png";
@@ -11,6 +12,7 @@ const Header = () => {
       <div className={styles.logo}><img alt={'logo'} src={logo}/></div>
       <h1>Бульба Курс</h1>
       <Navbar/>
+      <span className={styles.browserInfo}>Браузер: {browserName}</span>
     </header>
   );
 };
